9eb49a322a51f7cd4b44c3c92a0cad0e372b99e0
EmaWebSolutions <144553344+EmaWebSolutions@users.noreply.github.com>
2023-10-12 12:30:16 +0100

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -9,5 +9,8 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
+echo "Generating application key..."
+php artisan key:generate --show
+
 echo "Running migrations..."
 php artisan migrate --force
